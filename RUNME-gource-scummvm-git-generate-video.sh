5542d0f98ec85e94915f5113289f090805d0bb9b
D G Turner <digitall@scummvm.org>
2013-12-14 07:23:22 +0000

    MOVIE: Add script status as used for Youtube release by James Woodcock.
    The scummvm_logo_svg.png was composited to the video after generation in
    the video post production which also added the audio track.

--- RUNME-gource-scummvm-git-generate-video.sh
+++ RUNME-gource-scummvm-git-generate-video.sh
@@ -15,12 +15,14 @@
 # * FFMPEG with x264 library support
 
 # Frame Rate
-FRAMERATE=60
-#FRAMERATE=30
+#FRAMERATE=60
+FRAMERATE=30
 
 #Screen Size
+# 1080
+SCREENSIZE=1920x1080
 # 720p
-SCREENSIZE=1280x720
+#SCREENSIZE=1280x720
 # 800x600
 #SCREENSIZE=800x600
 # 640x480
@@ -28,7 +30,8 @@ SCREENSIZE=1280x720
 
 # Simulation Speed
 #SECSPERDAY=10
-SECSPERDAY=1
+#SECSPERDAY=1
+SECSPERDAY=0.1
 
 # Get ScummVM Git Repository
 if [ ! -d ./scummvm ]; then
@@ -61,7 +64,8 @@ echo "Sim Speed: ${SECSPERDAY}"
 echo "Framerate: ${FRAMERATE}"
 
 # Generate Video with Gource
-gource -${SCREENSIZE} --seconds-per-day ${SECSPERDAY} --user-image-dir .git/avatar/ --logo .git/avatar/scummvm_logo.jpg --caption-file caption.log --highlight-dirs --key --file-idle-time 0 --max-files 0 --stop-at-end --hide bloom,mouse,progress -r ${FRAMERATE} -o - | ffmpeg -y -r ${FRAMERATE} -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 ../scummvm.mp4
+gource -${SCREENSIZE} --seconds-per-day ${SECSPERDAY} --user-image-dir .git/avatar/  --caption-file caption.log --highlight-dirs --key --file-idle-time 0 --max-files 0 --stop-at-end --hide bloom,mouse,progress -r ${FRAMERATE} -o - | ffmpeg -y -r ${FRAMERATE} -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 0 -bf 0 ../scummvm.mp4
+# --logo .git/avatar/scummvm_logo.jpg
 
 # Clean up
 rm -f caption.log
